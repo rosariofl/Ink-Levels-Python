6aabec0d03673580d455dbdbd9e53d4df4ff0619
LuisR <79890342+rosariofl@users.noreply.github.com>
2022-02-11 09:07:38 -0700

    Uploading two dictionaries
    Adding dictionary containing printer names and IPs and color printer colors and values.

---
+++ color_printer_xpaths.py
@@ -0,0 +1,6 @@
+ink_colors = {
+    'Black':'//*[@id="inkLevel"]/tbody/tr[2]/td[1]/img',
+    'Cyan':'//*[@id="inkLevel"]/tbody/tr[2]/td[2]/img',
+    'Magenta':'//*[@id="inkLevel"]/tbody/tr[2]/td[3]/img',
+    'Yellow':'//*[@id="inkLevel"]/tbody/tr[2]/td[4]/img'
+}
---
+++ hp_printer_ip_list.py
@@ -0,0 +1,13 @@
+ips = {
+    "S8-HP404": "https://IP-ADDRESS",
+    "S2-HP404": "https://IP-ADDRESS",
+    "S3-HP404": "https://IP-ADDRESS",
+    "S4-HP404": "https://IP-ADDRESS",
+    "S5-HP404": "https://IP-ADDRESS",
+    "S6-HP404": "https://IP-ADDRESS",
+    "S7-HP404": "https://IP-ADDRESS",
+    "F1-HP404": "https://IP-ADDRESS",
+    "F2-HP404": "https://IP-ADDRESS",
+    "F3-HP404": "https://IP-ADDRESS",
+    "Freight Printer": "https://IP-ADDRESS"
+}
